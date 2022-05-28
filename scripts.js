f49674236228b91d31dbb5241adea59b660dce77
Ivan Knysh <mr.rauber.ik@gmail.com>
2022-05-28 14:11:36 +0300

    Send data to server example

--- scripts.js
+++ scripts.js
@@ -1,24 +1,51 @@
 'use strict'
 
-const select = document.querySelector('#cars');
-const output = document.querySelector('.output');
+const sendForm = () => {
+    const form = document.querySelector('form');
+    const user = form.querySelector('[name=user]');
+    const email = form.querySelector('[name=email]');
+    const message = document.querySelector('.message');
 
-select.addEventListener('change', () => {
-    const request = new XMLHttpRequest();
+    const messages = {
+        'success': 'Данные успешно отправлены',
+        'warning': 'Данные отправляются',
+        'error': 'Ошибка отправки данных'
+    };
 
-    request.addEventListener('readystatechange', () => {
-        if (request.readyState === 4 && request.status === 200) {
-            const data = JSON.parse(request.responseText);
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-            data.cars.forEach(item => {
-                if (item.brand === select.value) {
-                    output.innerHTML = 'Model: ' + item.model + '<br> Price: ' + item.price;
-                }
-            })
-        }
+        const request = new XMLHttpRequest();
+
+        request.addEventListener('readystatechange', () => {
+            message.classList.add('warning');
+            message.textContent = messages.warning;
+
+            if (request.readyState !== 4) {
+                return;
+            }
+
+            if (request.status === 200) {
+                message.classList.remove('warning');
+                message.classList.add('success');
+                message.textContent = messages.success;
+            } else {
+                message.classList.remove('warning');
+                message.classList.add('error');
+                message.textContent = messages.error;
+            }
+        });
+        request.open('POST', './send.php');
+        request.setRequestHeader('Content-type', 'application/json');
+
+        const data = {};
+        data.user = user.value;
+        data.email = email.value;
+
+        const body = JSON.stringify(data);
+
+        request.send(body);
     });
+}
 
-    request.open('GET', './cars.json');
-    request.setRequestHeader('Content-Type', 'application/json');
-    request.send();
-});
+sendForm();
