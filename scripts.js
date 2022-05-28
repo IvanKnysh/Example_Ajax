7e5e3d1a5b6c9c64b85a0a7ab5a265e1cb0469e4
Ivan Knysh <mr.rauber.ik@gmail.com>
2022-05-28 12:29:15 +0300

    Done work with 'XMLHttpRequest'

--- scripts.js
+++ scripts.js
@@ -1,2 +1,24 @@
 'use strict'
 
+const select = document.querySelector('#cars');
+const output = document.querySelector('.output');
+
+select.addEventListener('change', () => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener('readystatechange', () => {
+        if (request.readyState === 4 && request.status === 200) {
+            const data = JSON.parse(request.responseText);
+
+            data.cars.forEach(item => {
+                if (item.brand === select.value) {
+                    output.innerHTML = 'Model: ' + item.model + '<br> Price: ' + item.price;
+                }
+            })
+        }
+    });
+
+    request.open('GET', './cars.json');
+    request.setRequestHeader('Content-Type', 'application/json');
+    request.send();
+});
